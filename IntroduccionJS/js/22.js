64eb5e2ac09018b7d45bd9f5a6c6c9f9ebfe7020
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-19 20:20:31 -0300

    Optional Chaining y Nullish Coalescing

---
+++ IntroduccionJS/js/22.js
@@ -0,0 +1,16 @@
+// Optional Chaining (?)
+const alumno = {
+  nombre: "Juan",
+  clase: "Programación 1",
+  aprobado: true,
+  examenes: {
+    examen1: 90,
+  },
+}
+
+console.log(alumno.examenes?.examen1) // Existe examenes?
+console.log("Despues de alumno")
+
+// Nullish coalescing operator (??)
+const pagina = 10 ?? 1
+console.log(pagina)
