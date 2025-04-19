54639112f636a43ffcf5f8150556c27123c0ae2a
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-19 12:31:38 -0300

    Function expression

---
+++ IntroduccionJS/js/14.js
@@ -0,0 +1,13 @@
+// Funciones - Function Expression
+
+// const sumar = function (numero1, numero2) {
+//   console.log(numero1 + numero2)
+// }
+
+function sumar(numero1, numero2) {
+  console.log(numero1 + numero2)
+}
+
+sumar(10, 20)
+sumar(300, 1)
+sumar(100)
