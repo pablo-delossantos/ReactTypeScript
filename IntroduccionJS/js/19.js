02d38f892921d7f9cd4352cd6e5fe86c64e10192
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-19 19:10:17 -0300

    Comparadores

---
+++ IntroduccionJS/js/19.js
@@ -0,0 +1,16 @@
+const numero1 = 20
+const numero2 = "20"
+
+/**
+  ==  Comparador no estricto
+  === Comparador estricto. Revisa valor y tipo de dato
+ */
+
+console.log(typeof numero1)
+console.log(typeof numero2)
+
+if (numero1 === numero2) {
+  console.log("Si, son iguales")
+} else {
+  console.log("No, no son iguales")
+}
