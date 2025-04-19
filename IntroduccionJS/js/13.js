a04745f38148d8c358573545aabf8db1ee8c8316
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-19 11:30:51 -0300

    Funciones

---
+++ IntroduccionJS/js/13.js
@@ -0,0 +1,9 @@
+// Funcrion Declaration
+
+function sumar(numero1, numero2) {          //parametros
+  console.log(numero1 + numero2)
+}
+
+sumar(10, 20)         //argumentos
+sumar(100, 131)
+sumar(10)
