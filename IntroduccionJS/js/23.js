8f7e076e1d72fce654d70a182d2461a69f34333b
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-19 20:27:12 -0300

    Evaluacion de corto circuito

---
+++ IntroduccionJS/js/23.js
@@ -0,0 +1,12 @@
+// Evaluacion de corto circuito
+
+// const auth = true
+
+// if (auth) {
+//   console.log("Truthy")
+// } else {
+//   console.log("Falsy")
+// }
+
+const auth = true
+auth && console.log("Usuario Autenticado")
