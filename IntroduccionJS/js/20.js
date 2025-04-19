9d957ffaf6fc4e63ce67197b1862947fa5c45bb2
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-19 19:22:40 -0300

    Logical Or y Logical And

---
+++ IntroduccionJS/js/20.js
@@ -0,0 +1,15 @@
+const saldo = 1000
+const pagar = 1200
+const tarjeta = true
+
+/**
+ * Logical Or y Logical And
+ * || Or - Al menos una se cumpla
+ * && And - Revisa que todas se cumplan
+ */
+
+if (saldo > pagar || tarjeta) {
+  console.log("Puedes pagar")
+} else {
+  console.log("No puedes pagar")
+}
