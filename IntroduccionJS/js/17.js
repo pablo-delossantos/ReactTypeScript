91ea7d407dbcc3e18d9297ba4ee19e590e80b9c2
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-19 17:13:10 -0300

    Fix prettier config

--- IntroduccionJS/js/17.js
+++ IntroduccionJS/js/17.js
@@ -5,16 +5,12 @@ const numeros = [10, 20, 30]
 
 // Filter
 // const nuevoArray = tecnologias.filter((tech) => tech !== 'HTML')
-const resultado = numeros.filter((numero) => numero > 15)
-if (resultado.length > 0) {
-  console.log("Si hay elementos")
-} else {
-  console.log("No hay elementos")
-}
+// const resultado = numeros.filter((numero) => numero > 15)
+
 // Includes
 // const resultado = tecnologias.includes('CSS')
 
 // Some - Devuelve si al menos uno cumple la condicion
-// const resultado = numeros.some(numero => numero > 15)
+// const resultado = numeros.some((numero) => numero > 15)
 
-console.log(resultado)
+// Find - Devuelve el primer elemento que cimple una funcion.
