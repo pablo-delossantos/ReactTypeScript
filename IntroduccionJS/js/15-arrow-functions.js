c5424d21d82dac6ecba027f84b180af1a61e15ee
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-19 13:15:30 -0300

    Arrow functions

---
+++ IntroduccionJS/js/15-arrow-functions.js
@@ -0,0 +1,9 @@
+// Funciones - Arrow Functions
+const sumar = (numero1 = 0, numero2 = 0) => console.log(numero1 + numero2)
+
+
+sumar(10, 20)
+sumar(300, 1)
+sumar(100)
+
+// Solamente las podemos usar en expresiones
