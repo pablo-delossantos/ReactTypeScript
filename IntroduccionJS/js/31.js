56dd3f4d2e9bb067e04dd585b101d690384df79f
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-20 19:22:09 -0300

    Feat: evento input

---
+++ IntroduccionJS/js/31.js
@@ -0,0 +1,17 @@
+// Eventos de l DOM - Inputs
+
+const inputNombre = document.querySelector("#nombre")
+inputNombre.addEventListener("input", e => {
+    console.log(e.target.value)
+})
+
+const inputPassword = document.querySelector("#password")
+inputPassword.addEventListener("input", functionPassword)
+
+function functionPassword() {
+    inputPassword.type = "text"
+
+    setTimeout(() => {
+        inputPassword.type = "password"
+    }, 300)
+}
