2536e43befb964ac759d023c841424d9e7659a4a
Pablo de los Santos <estudiodedalo@gmail.com>
2025-04-20 13:50:48 -0300

    Feat: Async - Await

--- IntroduccionJS/js/26.js
+++ IntroduccionJS/js/26.js
@@ -14,10 +14,13 @@ const url = "https://jsonplaceholder.typicode.com/comments";
 const consultarApi = async () => {
     try {
         const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Hubo un error...");
+        }
         const data = await response.json();
         console.log(data);
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
     }
 };
 
